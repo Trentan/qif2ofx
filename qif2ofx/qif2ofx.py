0f1a893f4f3b9f38975a4146c4e61b21db690f6e
Trentan Healey <trentanh@gmail.com>
2023-12-24 23:23:30 +1000

    Added transaction amount reverse for savings

--- qif2ofx/qif2ofx.py
+++ qif2ofx/qif2ofx.py
@@ -13,12 +13,14 @@ from ofxtools.utils import UTC
 from .qif import QIFFile
 
 
-def qif_to_stmttrn(qif_file):
+def qif_to_stmttrn(qif_file, savings):
     stmttrns = []
     for transaction in qif_file.transactions:
-        trntype = 'DEBIT' if transaction.amount < 0 else 'CREDIT'
         dtposted = transaction.date.replace(tzinfo=UTC)
         trnamt = transaction.amount
+        if(savings): # Reverse transaction if savings!
+            trnamt = trnamt*-1
+        trntype = 'DEBIT' if trnamt < 0 else 'CREDIT'
         name = transaction.payee
         fitid = '{}{}{}'.format(dtposted, trnamt, name)
         stmttrns.append(m.STMTTRN(
@@ -32,25 +34,36 @@ def qif_to_stmttrn(qif_file):
     return stmttrns
 
 
-def genofx(qif_file, fileDir, currency, acctid, trnuid, org, balance):
-    trans = qif_to_stmttrn(qif_file)
+def genofx(qif_file, fileDir, currency, acctid, trnuid, org, balance, accttype):
+    trans = qif_to_stmttrn(qif_file, accttype.upper() == "SAVINGS")
 
     balamt = Decimal(balance) + qif_file.balance()
-    ledgerbal = m.LEDGERBAL(balamt=balamt,
-                            dtasof=qif_file.last_transaction_date())
-    ccacctfrom = m.CCACCTFROM(acctid=acctid)  # OFX Section 11.3.1
+    ledgerbal = m.LEDGERBAL(balamt=balamt, dtasof=qif_file.last_transaction_date())
+    fi = m.FI(org=org, fid='666')  # Required for Quicken compatibility
     banktranlist = m.BANKTRANLIST(*trans, dtstart=datetime(2019, 1, 1, 12, tzinfo=UTC),
                                   dtend=datetime(2018, 1, 1, 12, tzinfo=UTC))
     status = m.STATUS(code=0, severity='INFO')
-    ccstmtrs = m.CCSTMTRS(curdef=currency, ccacctfrom=ccacctfrom, banktranlist=banktranlist, ledgerbal=ledgerbal)
-    ccstmttrnrs = m.CCSTMTTRNRS(trnuid=trnuid, status=status, ccstmtrs=ccstmtrs)
-    ccmsgsrsv1 = m.CREDITCARDMSGSRSV1(ccstmttrnrs)
-    fi = m.FI(org=org, fid='666')  # Required for Quicken compatibility
     sonrs = m.SONRS(status=status,
                     dtserver=datetime.now(tz=UTC),
                     language='ENG', fi=fi)
     signonmsgs = m.SIGNONMSGSRSV1(sonrs=sonrs)
-    ofx = m.OFX(signonmsgsrsv1=signonmsgs, creditcardmsgsrsv1=ccmsgsrsv1)
+
+    if(accttype.upper() == "SAVINGS"):
+        # Savings account
+        bankacctfrom = m.BANKACCTFROM(bankid=org, acctid=acctid, accttype="SAVINGS")  # OFX Section 11.3.1
+
+        stmtrs = m.STMTRS(curdef=currency, bankacctfrom=bankacctfrom, banktranlist=banktranlist, ledgerbal=ledgerbal)
+        stmttrnrs = m.STMTTRNRS(trnuid=trnuid, status=status, stmtrs=stmtrs)
+        bankmsgsrsv1 = m.BANKMSGSRSV1(stmttrnrs)
+        ofx = m.OFX(signonmsgsrsv1=signonmsgs, bankmsgsrsv1=bankmsgsrsv1)
+    else:
+        # # Credit card
+        ccacctfrom = m.CCACCTFROM(acctid=acctid)  # OFX Section 11.3.1
+        ccstmtrs = m.CCSTMTRS(curdef=currency, ccacctfrom=ccacctfrom, banktranlist=banktranlist, ledgerbal=ledgerbal)
+        ccstmttrnrs = m.CCSTMTTRNRS(trnuid=trnuid, status=status, ccstmtrs=ccstmtrs)
+        ccmsgsrsv1 = m.CREDITCARDMSGSRSV1(ccstmttrnrs)
+        ofx = m.OFX(signonmsgsrsv1=signonmsgs, creditcardmsgsrsv1=ccmsgsrsv1)
+
     root = ofx.to_etree()
     message = ET.tostring(root).decode()
     pretty_message = minidom.parseString(message).toprettyxml()
@@ -75,18 +88,24 @@ def main():
                         default="BankOfEvil")
     parser.add_argument('--balance', required=False, help='Start Balance of the QIF files, or zero if unspecified',
                         default='0')
+    parser.add_argument('--accttype', required=False, help='Type of account, example: SAVINGS. "CD" if not supplied',
+                        default='CD')
 
     args = parser.parse_args()
 
     os.chdir(args.glob)
     for file in glob("*.qif"):
-        print(os.path.splitext(file)[0])
+        # print(os.path.splitext(file)[0])
         fileName = os.path.splitext(file)[0]
         match fileName:
             case fileName if "Qif" in fileName:
                 args.org = "Suncorp"
+                args.acctid = "SuncorpMain"
+                args.accttype="SAVINGS"
             case fileName if "TranHist" in fileName:
                 args.org = "HSBC"
+                args.acctid = "HSBCcc"
+                args.accttype="CD"
 
         qif = QIFFile.parse_files(file)
         # print(
@@ -97,6 +116,7 @@ def main():
             args.acctid,
             args.trnuid,
             args.org,
-            args.balance
+            args.balance,
+            args.accttype
         )
     # )
